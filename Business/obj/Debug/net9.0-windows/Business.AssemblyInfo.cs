6d6f8045b168e4b06bc77799470c87b8dba10a86
ZaidAlhassnawi <zaidhany0@outlook.com>
2025-03-16 23:46:34 +0300

    Add

--- Business/obj/Debug/net9.0-windows/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net9.0-windows/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25acc02d78d7f6897e01c4cd8b5534f65490de2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e5797f665e054052a026dbb337b6fa252e2ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
